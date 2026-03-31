0667c21361867dff40b9738243e5a8ee34523c7f
mzhangxy <96800168+mzhangxy@users.noreply.github.com>
2026-03-31 21:18:54 +0800

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const NEZHA_KEY = process.env.NEZHA_KEY || '';             // 哪吒v1的NZ_CLIE
 const ARGO_DOMAIN = process.env.ARGO_DOMAIN || 'giga.colorrora.com';          // 固定隧道域名,留空即启用临时隧道
 const ARGO_AUTH = process.env.ARGO_AUTH || 'eyJhIjoiZDY1NWNiOTk2NzNlZTYzMDE4NDFkMmQyNmYxNTY5N2EiLCJ0IjoiYzdjYWJiYjgtMWIyZS00NmUwLWI2ZTQtODliMTQ1M2ViOTgyIiwicyI6Ik9XWTVaR1ZqTnpjdFkyVTFaUzAwT1RjekxUZzBaV0l0TXpRek0yUmlNelV4WXpVdyJ9';              // 固定隧道密钥json或token,留空即启用临时隧道,json获取地址：https://json.zone.id
 const ARGO_PORT = process.env.ARGO_PORT || 8001;            // 固定隧道端口,使用token需在cloudflare后台设置和这里一致
-const CFIP = process.env.CFIP || 'saas.sin.fan';            // 节点优选域名或优选ip  
+const CFIP = process.env.CFIP || 'store.ubi.com';            // 节点优选域名或优选ip  
 const CFPORT = process.env.CFPORT || 443;                   // 节点优选域名或优选ip对应的端口
 const NAME = process.env.NAME || 'GiGA';                        // 节点名称
 
